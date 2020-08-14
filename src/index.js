28020236fe3c2c07efb755189d4be72d39726f34
Fogrexon <fogrexon@gmail.com>
2020-08-14 11:17:03 +0900

    Merge branch 'front-init' into timeline 初期設定をアップデートする

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
 ReactDOM.render(
+  // eslint-disable-next-line react/jsx-filename-extension
   <App />,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
