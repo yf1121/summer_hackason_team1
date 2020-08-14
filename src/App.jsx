0dd3acc49b3026c8b6b126ce41d1439f94083db3
yf1121 <yfujioka1121@outlook.jp>
2020-08-14 18:15:32 +0900

    change passage

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const App = () => (
-  <h1>Hello, world!</h1>
+  <h2>新聞の感想を投稿する全く新しいアプリ</h2>
 );
 
 export default App;
