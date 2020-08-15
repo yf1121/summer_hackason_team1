9c0afacea2f888c342549ac0df729b540cd26496
yf1121 <yfujioka1121@outlook.jp>
2020-08-15 21:55:31 +0900

    fix post page design

--- src/post/main.jsx
+++ src/post/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {
-  Container,
   Form,
   Button,
   Alert,
+  Card,
 } from 'react-bootstrap';
 import { firebase, createPost, getCurrentUser } from '../utils/firebase/main';
+import style from './Post.module.css';
 
 import TagMenu from './TagMenu';
 import { tags, newspapers } from '../utils/tags';
@@ -59,28 +60,30 @@ export default class Post extends React.Component {
       title, content, newsday, isPost, newspaper, tag,
     } = this.state;
     return (
-      <Container className="maincontents">
-        {isPost ? <Alert variant="primary">Posted</Alert> : ''}
-        <Form>
-          <TagMenu tags={newspapers} value={newspaper} title="Newspaper" onChange={(_tag) => this.onChangeTag('newspaper', _tag)} />
-          <Form.Group controlId="postNewsday">
-            <Form.Label>NewsDate</Form.Label>
-            <Form.Control type="date" value={newsday} onChange={(e) => this.onChange(e, 'newsday')} />
-          </Form.Group>
-          <TagMenu tags={tags} value={tag} title="Tag" onChange={(_tag) => this.onChangeTag('tag', _tag)} />
-          <Form.Group controlId="postTitle">
-            <Form.Label>Post Title</Form.Label>
-            <Form.Control type="text" placeholder="Title" value={title} onChange={(e) => this.onChange(e, 'title')} />
-          </Form.Group>
-          <Form.Group controlId="postContent">
-            <Form.Label>Content</Form.Label>
-            <Form.Control as="textarea" placeholder="Content" value={content} onChange={(e) => this.onChange(e, 'content')} />
-          </Form.Group>
-          <Button variant="primary" type="submit" onClick={(e) => this.onClick(e)} disabled={isPost}>
-            Submit
-          </Button>
-        </Form>
-      </Container>
+      <div className={`${style.wrapper} maincontents`}>
+        <Card className={style.card}>
+          {isPost ? <Alert variant="primary">Posted</Alert> : ''}
+          <Form>
+            <TagMenu tags={newspapers} value={newspaper} title="Newspaper" onChange={(_tag) => this.onChangeTag('newspaper', _tag)} />
+            <Form.Group controlId="postNewsday">
+              <Form.Label>NewsDate</Form.Label>
+              <Form.Control type="date" value={newsday} onChange={(e) => this.onChange(e, 'newsday')} />
+            </Form.Group>
+            <TagMenu tags={tags} value={tag} title="Tag" onChange={(_tag) => this.onChangeTag('tag', _tag)} />
+            <Form.Group controlId="postTitle">
+              <Form.Label>Post Title</Form.Label>
+              <Form.Control type="text" placeholder="Title" value={title} onChange={(e) => this.onChange(e, 'title')} />
+            </Form.Group>
+            <Form.Group controlId="postContent">
+              <Form.Label>Content</Form.Label>
+              <Form.Control as="textarea" placeholder="Content" value={content} onChange={(e) => this.onChange(e, 'content')} />
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={(e) => this.onClick(e)} disabled={isPost}>
+              Submit
+            </Button>
+          </Form>
+        </Card>
+      </div>
     );
   }
 }
